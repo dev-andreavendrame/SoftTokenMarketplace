54a6f637f1072ba3fa1550c8737466846c1aca2f
dev.andreavendrame <dev.andreavendrame@gmail.com>
2023-11-11 22:26:02 +0100

    added simulation costs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-solhint");
+require("hardhat-gas-reporter");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -7,6 +9,15 @@ module.exports = {
 		hardhat: {
 			rpc: "http://127.0.0.1:8545/",
 		},
+	},
+	gasReporter: {
+		enabled: true,
+		outputFile: "gasReport.txt",
+		noColors: true,
+		gasPrice: 200, // Simulate congested network
+		token: "MATIC",
+		gasPriceApi: "https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice",
+		coinmarketcap: process.env.COINMARKETCAP_KEY,
 	},
 
 	solidity: "0.8.20",
